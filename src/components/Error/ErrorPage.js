1976ae608290c8fdc349c7ca30a8506169a4bb64
Hardy Sylvestre <hardsyldev@gmail.com>
2023-11-12 16:20:33 +0300

    style error page

--- src/components/Error/ErrorPage.js
+++ src/components/Error/ErrorPage.js
@@ -1,9 +1,24 @@
 import React from "react";
+import batman from "../../images/batman.png";
+
+
+const centerH2 = {
+  textAlign: "center",
+  marginTop: '50px'
+}
+
+const image = {
+  display: 'block',
+  margin: "40px auto"
+}
 
 const ErrorPage = () => {
   return (
     <div className="quizz-bg">
-      <div className="container">ErrorPage</div>
+      <div className="container">
+        <h2 style={centerH2}>Oups, cette page n'existe pas</h2>
+        <img style={image} src={batman} alt="batman" />
+      </div>
     </div>
   );
 };
