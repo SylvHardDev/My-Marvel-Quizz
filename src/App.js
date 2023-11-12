bfd62d073cf0a266ae1158dfc5eb0bdc19d58516
Hardy Sylvestre <hardsyldev@gmail.com>
2023-11-12 14:56:43 +0300

    initialisation projet marvel quizz

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header"; 
+import Landing from "./components/Landing/Landing";
+import Footer from "./components/Footer/Footer.js";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Landing/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-container">
+        <p>Projet Marvel de Donkey, Restyled by SylvHardDev</p>
+        <p>Images: IconFinder</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
