e03488a632963f538d1e54e6e1591f3b5d4a5262
Hardy Sylvestre <hardsyldev@gmail.com>
2023-11-12 15:52:55 +0300

    add react-router-dom

--- src/components/Error/ErrorPage.js
+++ src/components/Error/ErrorPage.js
@@ -3,7 +3,7 @@ import React from "react";
 const ErrorPage = () => {
   return (
     <div className="quizz-bg">
-      <div className="container">Signup</div>
+      <div className="container">ErrorPage</div>
     </div>
   );
 };
